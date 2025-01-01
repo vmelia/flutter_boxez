f2c7824e82cb0767c98771730b4afd4fe87e07e9
Vince Melia <vince@drifta.co.uk>
2025-01-01 15:47:07 +0000

    Tidy up.

--- lib/helpers/colours.dart
+++ lib/helpers/colours.dart
@@ -3,15 +3,8 @@ import 'package:flutter/material.dart';
 class Colours {
   static MaterialColor convertToColour(int colour) => _colours[colour];
 
-  static int convertFromColour(MaterialColor colour) {
-    for (var i = 0; i < _colours.length; i++) {
-      if (_colours[i] == colour) {
-        return i;
-      }
-    }
-    return -1; //Error.
-  }
-
+  static int get count => _colours.length;
+  
   static const List<MaterialColor> _colours = <MaterialColor>[
     Colors.red,
     Colors.orange,
@@ -20,6 +13,4 @@ class Colours {
     Colors.blue,
     Colors.purple,
   ];
-
-  static int get count => _colours.length;
 }
--- lib/types/constants.dart
+++ lib/types/constants.dart
@@ -4,9 +4,9 @@ class Constants {
   static const double gridEnd = 3.0;
   static const double gridSize = gridEnd - gridStart + 1;
   static const double cellOffset = 2.0;
-   
+
   // BoxWidgets.
   static const double boxPadding = 1.0;
-  static const double boxRadius = 20.0;
+  static const double boxRadius = 30.0;
   static const int boxGradientAlpha = 100;
 }
